f09fcecb1689d138094702df4039e5e26918dfbd
ThibautG <6084433+ThibautG@users.noreply.github.com>
2025-08-28 16:46:58 +0200

    gestion connexion db

---
+++ database.py
@@ -0,0 +1,23 @@
+import pymongo
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_connection():
+  try:
+    uri = os.getenv('DB_URI')
+    client = MongoClient(uri)
+
+    database = client[os.getenv('DB_DATABASE')]
+    collection = database[os.getenv('DB_COLLECTION')]
+
+    # start example code here
+    print("Database connected successfully")
+    # end example code here
+    return client, database, collection
+
+  except Exception as e:
+      raise Exception(
+          "The following error occurred: ", e)
