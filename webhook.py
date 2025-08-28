2fe122347f49a2a20e40c6431022d19268f5b0ab
ThibautG <6084433+ThibautG@users.noreply.github.com>
2025-08-28 14:35:05 +0200

    envoi message vers webhook discord

---
+++ webhook.py
@@ -0,0 +1,27 @@
+import os
+
+import requests # dependency
+import os
+from dotenv import load_dotenv
+
+def send_msg_discord ( username, content ) :
+  load_dotenv()
+  url = os.getenv('WEBHOOK_URL') # webhook url, from here:
+  # https://i.imgur.com/f9XnAew.png
+
+  # for all params, see https://discordapp.com/developers/docs/resources/webhook#execute-webhook
+  data = { "username": username, "content": content }
+
+  # leave this out if you dont want an embed
+  # for all params, see https://discordapp.com/developers/docs/resources/channel#embed-object
+
+  result = requests.post(url, json = data)
+
+  try:
+      result.raise_for_status()
+  except requests.exceptions.HTTPError as err:
+      print(err)
+  else:
+      print(f"Payload delivered successfully, code {result.status_code}.")
+
+  # result: https://i.imgur.com/DRqXQzA.png
