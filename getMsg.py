7ccc3348329f3aa049ac25c8744dfd115025b90e
ThibautG <6084433+ThibautG@users.noreply.github.com>
2025-08-28 16:48:50 +0200

    envoi info reçu par mqtt dans db

--- getMsg.py
+++ getMsg.py
@@ -3,22 +3,34 @@ import paho.mqtt.client as mqtt
 import os
 from dotenv import load_dotenv
 from webhook import send_msg_discord
+from database import get_connection
 
 load_dotenv()
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, reason_code, properties):
-    print(f"Connected with result code {reason_code}")
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe(os.getenv('MQTT_SUBSCRIBE'))
+  print(f"Connected with result code {reason_code}")
+  # Subscribing in on_connect() means that if we lose the connection and
+  # reconnect then subscriptions will be renewed.
+  client.subscribe(os.getenv('MQTT_SUBSCRIBE'))
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-    data = json.loads( msg.payload.decode( "utf-8" ) )
-    decoded = data[ "uplink_message" ][ "decoded_payload" ]
-    send_msg_discord( msg.topic, str( decoded ) )
+  client, database, collection = get_connection()
+
+  print( msg.topic + " " + str( msg.payload ) )
+
+  # Décodage JSON
+  data = json.loads( msg.payload.decode( "utf-8" ) )
+  #decoded = data[ "uplink_message" ][ "decoded_payload" ]
+
+  # Enregistrement dans la collection MongoDB
+  result = collection.insert_one(data)
+  print( "Insertion MongoDB OK:", result.acknowledged )
+
+  # Envoi sur Discord
+  #send_msg_discord( msg.topic, str( decoded ) )
+
 # Création d'un client MQTT
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 # Configuration du nom d'utilisateur et du mot de passe
