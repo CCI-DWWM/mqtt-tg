d1fce27dafe6fa1a0c5d54fb592ae6bc6269c4ce
ThibautG <6084433+ThibautG@users.noreply.github.com>
2025-08-29 14:14:27 +0200

    lister les messages reçus dans la DB

--- listerMsg.py
+++ listerMsg.py
@@ -4,4 +4,4 @@ client, database, collection = get_connection()
 
 docs = collection.find({"end_device_ids.device_id":"bridge-chaumont"})
 for doc in docs:
-    print(doc.get("received_at"), doc.get('end_device_ids').get('device_id'), doc.get('uplink_message').get('decoded_payload').get('haut'))
+  print(doc.get("received_at"), doc.get('end_device_ids').get('device_id'), doc.get('uplink_message').get('decoded_payload').get('haut'))
